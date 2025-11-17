8eb63ddc633ce92bbcd692107261a85c9d0fba9f
jingwenshang <jingwenshang0521@gmail.com>
2025-11-17 03:20:40 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useContext } from "react";
-import { AuthContext } from "./context/AuthContext";
+import { AuthContext } from "../context/AuthContext";
 
 const UploadForm = ({ triggerLogin }) => {
   const { token } = useContext(AuthContext);
